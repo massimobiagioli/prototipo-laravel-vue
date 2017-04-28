54a14437908cbeda6a1d6521524b422ca3d60a93
Massimo Biagioli <biagiolimassimo@gmail.com>
2017-04-28 07:32:30 +0200

    Aggiunta autenticazione JWT

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,21 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+    public function run() {
+        User::create([
+            'email' => 'dummy@libero.it' ,
+            'password' => Hash::make('asdf1234') ,
+            'name' => 'Utente Dummy' ,
+        ]);
     }
+
 }
